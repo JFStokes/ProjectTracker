e4ece8b3a09fc21e539486fb1cc3b3240b152647
HackerSacker <117953457+JFStokes@users.noreply.github.com>
2025-11-10 17:38:55 -0600

    Added highlight color for progress bars.

--- Utilities.cs
+++ Utilities.cs
@@ -10,21 +10,20 @@ public class Utilities
         int projectDaysBetween = (proj.GoalDate.ToDateTime(TimeOnly.MinValue) - proj.StartDate.ToDateTime(TimeOnly.MinValue)).Days; // Gets total days of project.
         int timeDaysBetween = (proj.GoalDate.ToDateTime(TimeOnly.MinValue) - today.ToDateTime(TimeOnly.MinValue)).Days; // Gets number of days from Today to end of project.
         int daysPast = projectDaysBetween - timeDaysBetween; // Gets Days since project started.
-        // int daysPast = 2; // Place Filler for real var.
-        DrawBar(daysPast, projectDaysBetween);
-        int intPercent = (int)Math.Round((double)daysPast / projectDaysBetween * 100.0);
-        Console.Write($"Days: {daysPast}/{projectDaysBetween}\t{intPercent}%\n");
+        DrawBar(daysPast, projectDaysBetween, "days", 0, 0);
+        int intPercentDays = (int)Math.Round((double)daysPast / projectDaysBetween * 100.0);
+        Console.Write($"Days: {daysPast}/{projectDaysBetween}\t{intPercentDays}%\n");
 
         // Draw Completed Bar.
-        DrawBar(proj.CompletedTasks, proj.TotalTasks);
-        intPercent = (int)Math.Round((double)proj.CompletedTasks / proj.TotalTasks * 100.0);
-        Console.Write($"Tasks: {proj.CompletedTasks}/{proj.TotalTasks}\t{intPercent}%\n");
+        int intPercentTasks = (int)Math.Round((double)proj.CompletedTasks / proj.TotalTasks * 100.0);
+        DrawBar(proj.CompletedTasks, proj.TotalTasks, "tasks", intPercentDays, intPercentTasks);
+        Console.Write($"Tasks: {proj.CompletedTasks}/{proj.TotalTasks}\t{intPercentTasks}%\n");
 
         Console.Write("\n");
     }
 
     // ########## DRAW BAR ##########
-    public async static void DrawBar(int num, int den)
+    public async static void DrawBar(int num, int den, string type, int percDay, int percTask)
     {
         int intPercent = (int)Math.Round((double)num / den * 100.0);
         intPercent = (int)Math.Round((double)intPercent / 2.0);
@@ -32,7 +31,8 @@ public class Utilities
         Console.Write("|");
         for (int i = 0; i < intPercent; i++)
         {
-            Console.Write("X");
+            HighlightBar(type, percDay, percTask);
+            Console.ResetColor();
         }
 
         int intRemaining = 50 - intPercent;
@@ -43,7 +43,23 @@ public class Utilities
         Console.Write("|");
     }
 
-    // ########## HIGHLIGHT WHITE ##########
-    // ########## HIGHLIGHT GREEN ##########
-    // ########## HIGHLIGHT RED ##########
+    // ########## HIGHLIGHT BAR ##########
+    static void HighlightBar(string type, int percDay, int percTask)
+    {
+        if (type == "days")
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(" ");
+        }
+        else if (type == "tasks" && percTask >= percDay)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+        }
+        else if (type == "tasks" && percTask < percDay)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(" ");
+        }
+    }
 }
